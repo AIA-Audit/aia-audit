7e5dc84679a8b12250f16315a0d947e4b87c4fcd
Ism1tha <xgalan@hotmail.com>
2023-02-02 00:33:40 +0100

    Fix config structure error on load

--- aia_audit/lib/config.py
+++ aia_audit/lib/config.py
@@ -11,13 +11,13 @@ class Config:
         if not os.path.isfile("aia_audit/data/config.json"):
             print("[*] Creating config file ... Done!")
             with open("aia_audit/data/config.json", "w") as config_file:
-                config_file.write("{ website_address: \"" + self.website_address + "\", website_port: \"" + self.website_port + "\", website_password: \"" + self.website_password + "\" }")
+                config_file.write("{ \"website_address\": \"" + self.website_address + "\", \"website_port\": \"" + self.website_port + "\", \"website_password\": \"" + self.website_password + "\" }")
         else:
             print("[*] Config file already exists ... Done!")
             with open("aia_audit/data/config.json", "r") as config_file:
                 config = config_file.read()
                 #Extract values from json file
-                self.website_address = config.split("website_address: \"")[1].split("\",")[0]
-                self.website_port = config.split("website_port: \"")[1].split("\",")[0]
-                self.website_password = config.split("website_password: ")[1].split(" }")[0]
+                self.website_address = config.split("\"website_address\": \"")[1].split("\",")[0]
+                self.website_port = config.split("\"website_port\": \"")[1].split("\",")[0]
+                self.website_password = config.split("\"website_password\": \"")[1].split("\",")[0]
         
