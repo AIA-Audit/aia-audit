2f1f9a99280d80abf0a415a10a80421600085d92
Ism1tha <xgalan@hotmail.com>
2023-02-02 13:33:07 +0100

    API route returning data (Testing)

--- aia_audit/website/routes.py
+++ aia_audit/website/routes.py
@@ -1,7 +1,11 @@
-# Get the app object from the __init__.py file
 from aia_audit.website.engine import app
 from flask import render_template
+import aia_audit.__main__ as main
+import json
 
+
+
+#User Routes
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -9,3 +13,12 @@ def home():
 @app.route('/about')
 def about():
     return render_template('index.html')
+
+#API routes
+@app.route('/api/data/tool-info')
+def tool_info():
+    data = {
+        "status": main.status,
+        "version": main.version
+    }
+    return data
