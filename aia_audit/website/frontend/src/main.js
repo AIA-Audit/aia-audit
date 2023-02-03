d07c737c9cab9801faa9d0bb805f3850e9779e20
Ism1tha <xgalan@hotmail.com>
2023-02-03 16:03:54 +0100

    Install PrimeVue and Axios

--- aia_audit/website/frontend/src/main.js
+++ aia_audit/website/frontend/src/main.js
@@ -1,14 +1,21 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import axios from 'axios';
 import App from "./App.vue";
+import PrimeVue from 'primevue/config';
 import router from "./router";
 
 import "./assets/main.css";
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/tailwind-light/theme.css"
 
 const app = createApp(App);
 
+app.config.globalProperties.axios=axios;
+
 app.use(createPinia());
+app.use(PrimeVue);
 app.use(router);
 
 app.mount("#app");
